d72722dab4c45f889ececac452e2983effeacfed
heiyanquan <2792652898@qq.com>
2023-12-15 13:34:04 +0800

    table

--- packages/utils/src/index.tsx
+++ packages/utils/src/index.tsx
@@ -3,4 +3,5 @@ export { default as AsyncQueue } from './asyncQueue';
 export * from './constant';
 export * from './getUrlSearchParams';
 export * from './storage';
+export * from './table';
 export * from './tools';
