cae83659da02763038d1a21bc5d22aaf2d3a6036
heiyanquan <2792652898@qq.com>
2023-12-18 11:47:49 +0800

    version

---
+++ packages/components/src/version.ts
@@ -0,0 +1,12 @@
+export const version = {
+  '@hs-admin/api': '0.0.3',
+  '@react-admin/pro-components': '0.0.25',
+  '@react-admin/pro-input': '0.0.10',
+  '@react-admin/pro-page': '0.0.3',
+  '@react-admin/pro-select': '0.0.9',
+  '@react-admin/pro-spin': '0.0.4',
+  '@react-admin/pro-table': '0.0.3',
+  '@react-admin/pro-table-page': '0.0.4',
+  '@react-admin/pro-upload': '0.0.3',
+  '@react-admin/pro-utils': '0.0.11',
+};
