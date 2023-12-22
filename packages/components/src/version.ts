632e192c683800f169cf59db3731f18137c82968
吴洋 <2792652898@qq.com>
2023-12-22 18:25:16 +0800

    pro-components

--- packages/components/src/version.ts
+++ packages/components/src/version.ts
@@ -1,7 +1,7 @@
 export const version = {
   "@hs-admin/api": "0.0.7",
   "@react-admin/pro-codemirror": "0.0.4",
-  "@react-admin/pro-components": "0.0.35",
+  "@react-admin/pro-components": "0.0.36",
   "@react-admin/pro-form": "0.0.6",
   "@react-admin/pro-scroll-select": "0.0.3",
   "@react-admin/pro-spin": "0.0.5",
