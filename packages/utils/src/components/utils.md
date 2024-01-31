eb7234ba25cea4a01a29740838a62053dcd96421
吴洋 <2792652898@qq.com>
2024-01-31 11:58:54 +0800

    urlSearchParams

--- packages/utils/src/components/utils.md
+++ packages/utils/src/components/utils.md
@@ -6,9 +6,15 @@ title: utils - 工具方法
 
 ## 代码演示
 
+### 解析url参数
+
+<code src="../demos/urlSearchParams.tsx"  background="var(--main-bg-color)" oldtitle="常量"></code>
+
+
 ### 常量
 
 <code src="../demos/constant.tsx"  background="var(--main-bg-color)" oldtitle="常量"></code>
+
 
 ### 省市区数据
 
---
+++ packages/utils/src/demos/urlSearchParams.tsx
@@ -0,0 +1,14 @@
+import { getUrlSearchParams } from '@react-admin/pro-utils'
+
+export default () => {
+  const paramsString = 'https://datashop.aihuoshi.net/?q=URLUtils.searchParams&topic=api'
+  const q = getUrlSearchParams('q', paramsString)
+  const topic = getUrlSearchParams('topic', paramsString)
+
+  return (
+    <>
+      <h1>q：{q}</h1>
+      <h1>topic：{topic}</h1>
+    </>
+  )
+}
