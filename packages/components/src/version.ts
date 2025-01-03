6f978643b23934913c3938b56a0fad333be0fa31
heiyanquan <2792652898@qq.com>
2025-01-03 10:32:37 +0800

    input

--- packages/components/src/version.ts
+++ packages/components/src/version.ts
@@ -1,4 +1,4 @@
 export const version = {
-  '@zs-ai/components': '0.0.12',
-  '@zs-ai/input': '0.0.7'
+  '@zs-ai/components': '0.0.13',
+  '@zs-ai/input': '0.0.8'
 }
--- packages/input/src/index.tsx
+++ packages/input/src/index.tsx
@@ -1,9 +1,6 @@
-import type { InputType } from './typing'
-
 const ZsAiInput: any = (props: any) => {
-  return <input {...props} name="input" placeholder="请输入搜索文字1" />
+  return <input {...props} name="input" placeholder="请输入搜索文字" />
 }
 
-export type { InputType }
 export { ZsAiInput }
 export default ZsAiInput
