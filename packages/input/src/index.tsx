de7a82ba2727e11cf434813a2a176271fe6379e7
heiyanquan <2792652898@qq.com>
2025-01-03 10:18:06 +0800

    input

--- packages/input/src/index.tsx
+++ packages/input/src/index.tsx
@@ -1,7 +1,7 @@
 import type { InputType } from './typing'
 
 const ZsAiInput: any = (props: any) => {
-  return <input {...props} name="input" placeholder="请输入搜索文字" />
+  return <input {...props} name="input" placeholder="请输入搜索文字1" />
 }
 
 export type { InputType }
